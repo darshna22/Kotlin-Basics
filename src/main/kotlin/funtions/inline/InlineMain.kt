205f9bcd53d7440731a32c12d3cd22d3763a13c4
C5141506 <darshna.kumari@conduent.com>
2023-02-23 17:18:14 +0530

    first commit

--- src/main/kotlin/funtions/inline/InlineMain.kt
+++ src/main/kotlin/funtions/inline/InlineMain.kt
@@ -1,4 +1,18 @@
 package funtions.inline
 
-class InlineMain {
+
+fun main() {
+    val addFunc = { x: Int, y: Int -> x + y }
+    calculateTimAndRun(10, 20, addFunc)
+    calculateTimAndRun(10, 20, addFunc)
+    calculateTimAndRun(10, 20, addFunc)
+
+
+}
+
+ inline fun calculateTimAndRun(a: Int, b: Int, operation: (Int, Int) -> Int) {
+    val start = System.currentTimeMillis()
+    operation(a, b)
+    val end = System.currentTimeMillis()
+    println("${end-start}")
 }
--- src/main/kotlin/funtions/tailrecu/TailRecursionMain.kt
+++ src/main/kotlin/funtions/tailrecu/TailRecursionMain.kt
@@ -1,4 +1,22 @@
 package funtions.tailrecu
 
-class TailRecursionMain {
+import java.math.BigInteger
+
+
+fun main() {
+    val n = 10000
+    val first = BigInteger("0")
+    val second = BigInteger("1")
+    //println(generateFibonacciSeries(n, first, second))
+//    println(findFactorial(1000000))
+}
+
+tailrec fun generateFibonacciSeries(n: Int, a: BigInteger, b: BigInteger):BigInteger {
+    return if (n==0) a
+    else generateFibonacciSeries(n-1,b,a+b)
+}
+
+tailrec fun findFactorial(n: Int): Int {
+    return if (n == 1) n
+    else n * findFactorial(n - 1)
 }
