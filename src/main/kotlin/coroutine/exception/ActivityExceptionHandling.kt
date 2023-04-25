5e98309744fda12707eccf7a16f6c21f660c9529
C5141506 <darshna.kumari@conduent.com>
2023-04-25 15:39:44 +0530

    remove git ignore file

--- src/main/kotlin/coroutine/exception/ActivityExceptionHandling.kt
+++ src/main/kotlin/coroutine/exception/ActivityExceptionHandling.kt
@@ -1,27 +1,27 @@
-package coroutine.exception
-
-import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
-import kotlin.random.Random
-
-class ActivityExceptionHandling : CoroutineScope by CoroutineScope(SupervisorJob() + Dispatchers.Default) {
-//    override val coroutineContext: CoroutineContext
-//        get() = TODO("Not yet implemented")
-
-
-
-    fun onCreate() {
-        //all coroutines executing on supervisor scope
-        launch(coroutineContext) { task11() }
-        launch(coroutineContext) { task12()}
-        launch(coroutineContext) { task13()}
-        //CoroutineExceptionHandler exception use handle specific coroutine exception
-        launch(CoroutineExceptionHandler { coroutineContext, throwable ->  }) {
-            task11()
-        }
-    }
-
-    private fun task11() {}
-    private fun task12() {}
-    private fun task13() {}
-}
+//package coroutine.exception
+//
+//import kotlinx.coroutines.*
+//import kotlin.coroutines.CoroutineContext
+//import kotlin.random.Random
+//
+//class ActivityExceptionHandling : CoroutineScope by CoroutineScope(SupervisorJob() + Dispatchers.Default) {
+////    override val coroutineContext: CoroutineContext
+////        get() = TODO("Not yet implemented")
+//
+//
+//
+//    fun onCreate() {
+//        //all coroutines executing on supervisor scope
+//        launch(coroutineContext) { task11() }
+//        launch(coroutineContext) { task12()}
+//        launch(coroutineContext) { task13()}
+//        //CoroutineExceptionHandler exception use handle specific coroutine exception
+//        launch(CoroutineExceptionHandler { coroutineContext, throwable ->  }) {
+//            task11()
+//        }
+//    }
+//
+//    private fun task11() {}
+//    private fun task12() {}
+//    private fun task13() {}
+//}
